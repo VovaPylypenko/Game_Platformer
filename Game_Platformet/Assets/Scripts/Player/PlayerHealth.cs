a94b69d02f21274aa305779f25c92a13099aab66
VovaPylypenko <Pylypenko199813@gmail.com>
2019-12-19 22:31:54 +0200

    fix multiplayer & auto local hots

---
+++ Game_Platformet/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerHealth : NetworkBehaviour
+{
+    public int health = 100;
+    
+    //[SyncVar(hook = "OnHealthChanged")]
+    private int currentHealth;
+    
+    void Start()
+    {
+        currentHealth = health;
+    }
+    
+    public void TakeDamage(int damage)
+    {
+        if (!isServer)
+            return;
+
+        currentHealth -= damage;
+    }
+}
