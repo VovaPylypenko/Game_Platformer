6d2f2a2d90fb5fb83504b7be6920fe5648515b07
VovaPylypenko <Pylypenko199813@gmail.com>
2019-12-20 23:35:48 +0200

    fix bonus life

--- Game_Platformet/Assets/Scripts/Player/HealthBonus.cs
+++ Game_Platformet/Assets/Scripts/Player/HealthBonus.cs
@@ -7,22 +7,17 @@ public class HealthBonus : NetworkBehaviour
 {
 
     private int healthBonus = 2;
-
-    void OnTriggerEnter2D(Collider2D other)
+    
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log("OnTriggerEnter");
-        GetComponent<PlayerHealth>().health = GetComponent<PlayerHealth>().health + healthBonus;
-        Destroy(other.gameObject);
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("OnCollisionEnter");
-        if (collision.gameObject.name == "Player")
+        if (!isLocalPlayer)
         {
-            GetComponent<PlayerHealth>().health = GetComponent<PlayerHealth>().health + healthBonus;
-            Destroy(gameObject);
+            return;
+        }
+        if (other.gameObject.CompareTag("bonus"))
+        {
+            GetComponent<PlayerHealth>().UpHealth(healthBonus);
+            Destroy(other.gameObject);
         }
     }
-
 }
